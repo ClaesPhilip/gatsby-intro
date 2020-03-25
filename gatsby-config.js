37bd9e1debb13963f5ed978400be9c934e4916be
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-03-25 19:33:31 +0100

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    siteMetadata: {
+        title: 'Philips first Gatsby Project',
+        description: 'Philips first ever project using Gatsby, lets see how it goes!',
+
+    },
+    plugins: ['gatsby-plugin-emotion', 
+    'gatsby-plugin-react-helmet'],
+};
