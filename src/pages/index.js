b05e41dd02965c87ccc024fb5a887109fcca4f6f
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-03-25 19:34:25 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,12 @@
 import React from 'react';
+import {Link} from 'gatsby';
+import Layout from '../components/layout';
 
-export default () => <div>Hello Frontend Masters!</div>;
+
+export default () => ( 
+    <Layout>
+      <h1>Hi Frontend Masters!</h1>
+      <p>Hello Philip, Welcome</p>
+      <Link to='/about/'>Learn about us &larr;</Link>
+    </Layout>
+);
