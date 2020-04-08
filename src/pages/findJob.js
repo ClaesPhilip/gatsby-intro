8c01e949a3bb36ad40f812513a8d912da6b9b668
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-08 17:21:07 +0200

    Create findJob.js

---
+++ src/pages/findJob.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'gatsby';
+import Layout from '../components/layout';
+
+
+export default () => ( 
+    <Layout>
+      <h1>HITTA JOBB</h1>
+      
+      <Link to='/about/'>Learn about us &larr;</Link>
+    </Layout>
+);
