6918333099d0a966183fe6524809d9f96a13c3f4
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-08 17:21:11 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout';
 
 export default () => ( 
     <Layout>
-      <h1>Hi Frontend Masters!</h1>
+      <h1>LANDNINGSSIDAN</h1>
       <p>Hello Philip, Welcome</p>
       <Link to='/about/'>Learn about us &larr;</Link>
     </Layout>
