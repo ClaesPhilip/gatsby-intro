12a9a0fcabbe424a135a4343ffb0b145b07e224c
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2020-04-08 17:21:09 +0200

    Create hireStaff.js

---
+++ src/pages/hireStaff.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'gatsby';
+import Layout from '../components/layout';
+
+
+export default () => ( 
+    <Layout>
+      <h1>HYRA PERSONAL</h1>
+      
+      <Link to='/'>Click to home &larr;</Link>
+    </Layout>
+);
